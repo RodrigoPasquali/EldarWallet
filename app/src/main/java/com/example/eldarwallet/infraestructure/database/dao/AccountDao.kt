99b7f9db37b58e807c38d07e0405035ef5f8ff99
Rodrigo <pasquali.rodrigo@hotmail.com>
2023-08-30 15:28:06 -0300

    Saldo(feat): se agrega estrategia de ignorar en caso de que haya conflictos al momento de insertar una cuenta en la base

--- app/src/main/java/com/example/eldarwallet/infraestructure/database/dao/AccountDao.kt
+++ app/src/main/java/com/example/eldarwallet/infraestructure/database/dao/AccountDao.kt
@@ -2,12 +2,13 @@ package com.example.eldarwallet.infraestructure.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.eldarwallet.domain.model.Account
 
 @Dao
 interface AccountDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(account: Account)
 
     @Query("select balance from account_table where number = :number")
