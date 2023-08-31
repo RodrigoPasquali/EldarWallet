e526501c7914c772a4f7e63ec082f763ebc8e8a5
Rodrigo <pasquali.rodrigo@hotmail.com>
2023-08-31 17:26:36 -0300

    Login(feat): al presionar ingresar se pasa a HomeActivity

--- app/src/main/java/com/example/eldarwallet/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/eldarwallet/ui/login/LoginActivity.kt
@@ -1,12 +1,29 @@
 package com.example.eldarwallet.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.eldarwallet.R
+import com.example.eldarwallet.databinding.ActivityLoginBinding
+import com.example.eldarwallet.ui.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        onLoginButtonClick()
+    }
+
+    private fun onLoginButtonClick() {
+        binding.loginButton.setOnClickListener {
+            navigateToHome()
+        }
+    }
+
+    private fun navigateToHome() {
+        startActivity(Intent(this, HomeActivity::class.java))
     }
 }
